a0dbeb83b276b599a54ff3b55984abcbf955512a
JuliusUlee <61336116+Julius-Ulee@users.noreply.github.com>
2023-02-26 15:02:02 +0700

    Create source.js

---
+++ slashCommands/Info/source.js
@@ -0,0 +1,45 @@
+const {
+  MessageEmbed
+} = require("discord.js");
+const config = require("../../botconfig/config.json");
+var ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+module.exports = {
+  name: "source", //the command name for execution & for helpcmd [OPTIONAL]
+  cooldown: 5, //the command cooldown for execution & for helpcmd [OPTIONAL]
+  description: "Sends you Source Code Information", //the command description for helpcmd [OPTIONAL]
+  memberpermissions: [], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
+  requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+  alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+  run: async (client, interaction) => {
+    try {
+      //things u can directly access in an interaction!
+      const {
+        member,
+        channelId,
+        guildId,
+        applicationId,
+        commandName,
+        deferred,
+        replied,
+        ephemeral,
+        options,
+        id,
+        createdTimestamp
+      } = interaction;
+      const {
+        guild
+      } = member;
+      interaction.reply({
+        ephemeral: true,
+        embeds: [
+          new MessageEmbed().setColor(ee.color)
+          .setFooter(ee.footertext, ee.footericon)
+          .setDescription(`**SOURCE CODE** :heart:\n\n[Link to Source](https://github.com/Julius-Ulee/AmeliaBot-Discord)`)
+        ]
+      });
+    } catch (e) {
+      console.log(String(e.stack).bgRed)
+    }
+  }
+}
